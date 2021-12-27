41054d8d884352c2187fca904491ba1095b47baa
Fabian Mendez <32204060+fabianMendez@users.noreply.github.com>
2021-12-27 18:36:53 -0500

    fix: add charset to error response

--- cmd/functions/confirm_subscription/main.go
+++ cmd/functions/confirm_subscription/main.go
@@ -18,9 +18,22 @@ func handler(ctx context.Context, request events.APIGatewayProxyRequest) (*event
 		return &events.APIGatewayProxyResponse{
 			StatusCode: http.StatusBadRequest,
 			Headers: map[string]string{
-				"Content-Type": "text/html",
+				"Content-Type": "text/html; charset=utf-8",
 			},
-			Body: "<h1>Suscripción no encontrada</h1>",
+			Body: `
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+	<h1>Suscripción no encontrada</h1>
+</body>
+</html>
+		`,
 		}, nil
 	}
 
